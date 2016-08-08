6f0c80f8dbd4cc36c08f54525372b31c0ddc26af
mcgeeaw <mcgeeaw@gmail.com>
2016-08-08 16:31:02 -0500

    Update SpringbootStarterActuatorTestApplication.java

--- src/main/java/com/example/SpringbootStarterActuatorTestApplication.java
+++ src/main/java/com/example/SpringbootStarterActuatorTestApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootStarterActuatorTestApplication {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootStarterActuatorTestApplication.class, args);
