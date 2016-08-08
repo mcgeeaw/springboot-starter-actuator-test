e0520dd0eb863c538e343eef747bbd50f2f77052
unknown <mcgeeaw@07009MCGEEAW7.ad.ipacc.com>
2016-08-08 15:49:27 -0500

    Initial commit

---
+++ src/main/java/com/example/SpringbootStarterActuatorTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterActuatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterActuatorTestApplication.class, args);
+	}
+}
